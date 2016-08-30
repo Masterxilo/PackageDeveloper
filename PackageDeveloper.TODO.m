4ef6d411a0a811d328fa1476278cafa885ba5577
Paul Frischknecht <paul_frischknecht@bluewin.ch>
2016-08-30 10:11:26 +0200

    moved to  SavedExpressions

---
+++ PackageDeveloper.TODO.m
@@ -0,0 +1,23 @@
+(* ::Package:: *)
+
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: PackageDeveloper *)
+(* :Context: PackageDeveloper` *)
+(* :Author: Paul *)
+(* :Date: 2016-08-08 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2016 Paul *)
+(* :Keywords: *)
+(* :Discussion:
+Contains various meta commands to make package development easier.
+*)
+
+(* TODO error checking
+TODO support = definitions
+TODO allow hiding internal operations such as destructuring of data if desired (easier to understand)
+
+TODO don't force usage message for different internal variants: maybe separate internally? overhead!*)
